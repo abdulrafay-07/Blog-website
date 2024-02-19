74b907cb87aa5e96454398ca80679c3824f3c1db
Abdul Rafay <152396110+abdulrafay-07@users.noreply.github.com>
2024-02-19 14:42:46 +0500

    Completed banner component

---
+++ src/components/Banner.jsx
@@ -0,0 +1,22 @@
+import { Link } from 'react-router-dom';
+import { FaArrowRight } from "react-icons/fa6"
+
+const Banner = () => {
+    return (
+        <div className="px-4 py-32 bg-black mx-auto">
+            <div className="text-white text-center">
+                <h1 className="text-5xl lg:text-7xl leading-snug font-bold mb-5">Welcome to our Blog</h1>
+                <p className="text-gray-100 lg:w-3/5 mx-auto mb-5 font-primary">Start your blog today and join a community of writes and readers who are passionate about sharing their stories and ideas.</p>
+                <div>
+                    <Link 
+                        to="/about" 
+                        className="font-medium inline-flex items-center py-1 hover:text-orange-500">
+                        Learn more <FaArrowRight className='mt-1 ml-2'/>
+                    </Link>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Banner;
